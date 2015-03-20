82806c8724b110b445a6093904b35d986de9c00c
ballesterus <chuuuuy@gmail.com>
2015-03-20 02:02:18 -0400

    Developing SQLite DB

--- orthoGroups/myOrtho.py
+++ orthoGroups/myOrtho.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from sys import argv
 import re
 import os
@@ -9,34 +10,101 @@ import sqlite3
 """
 
 #PART I: where we incorportate all soyce seqeunces into a sqlite3 database
-conn = sqlite3.connect('sequences.db')
-c = c.cursor()
-c.execute('''CREATE TABLE trasncripts
-(transcriptID INTEGER PRIMARY KEY UNIQUE,
-sequence TEXT,
-trinity_identifier TEXT,
-len INT,
-path TEXT,
-genus TEXT,
-species TEXT,
-tissue TEXT)
-''')
+conn = sqlite3.connect('masterDB.sql')
+c = conn.cursor()
+c.execute("PRAGMA foreign_keys = ON;")
+c.execute('''CREATE TABLE IF NOT EXISTS Species
+(Species_ID INTEGER PRIMARY KEY UNIQUE,
+Genus TEXT,
+Species TEXT)''')
 
-#Feed the database
-file = raw_input('provide file name: ')
-infile = open(file, 'r')
+c.execute('''CREATE TABLE IF NOT EXISTS Transcrip
+(Transcrip_ID  INTEGER PRIMARY KEY UNIQUE,
+FastaHeader TEXT,
+FastaSeq TEXT,
+Species_ID INTEGER,
+FOREIGN KEY(Species_ID) REFERENCES Species(Species_ID)
+);''')
 
-def one_line_entry(file):
-    temp = open('temp_out.txt', 'rw')
-    for line in file:
-        if re.search(r'^>', line):
-            nl= re.sub(r'^(.+)\n', '\\1    ', line)
-            temp.write(nl)
-        else:
-            sle_source1.write(re.sub(r'\n', '', line))
-            sle_source1= open("sle.fa", 'r')
-            sle_ready = open('sle_ready.fa', 'w')
-            for line in sle_source1:
-                nl = re.sub(r'>', "\\n>", line)
-                sle_ready.write(nl)
-os.remove("sle1.fa")
+c.execute('''CREATE TABLE IF NOT EXISTS CDS
+(CDS_ID INTEGER PRIMARY KEY UNIQUE,
+FastaHeader TEXT,
+SeqAA TEXT,
+SeqNT TEXT,
+STranscrip INTEGER,
+FOREIGN KEY(STranscrip) REFERENCES Transcrip(Transcrip_ID)
+);''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Annotation
+(Annotation_ID  INTEGER PRIMARY KEY UNIQUE,
+FOREIGN KEY(Annotation_ID) REFERENCES CDS(CDS_ID)
+);''')
+
+
+c.execute('''CREATE TABLE IF NOT EXISTS Orthology
+(Ortho_ID INTEGER PRIMARY KEY UNIQUE,
+OrthoName TEXT,
+OrthoMethod TEXT
+);''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS CDS_has_Orthology
+(Ortho_ID INTEGER,
+CDS_ID INTEGER,
+FOREIGN KEY(Ortho_ID) REFERENCES Orthology(OrthoID),
+FOREIGN KEY(CDS_ID) REFERENCES CDS(CDS_ID)
+);''')
+
+
+class FastaRecord():
+    """Class for storing sequence records and related data"""
+    def __init__(self, IdLine):
+        self.SeqId = IdLine.replace('\n', '').strip('>')
+    
+
+def is_ID(Line):
+    if Line.startswith('>'):
+        return True
+    else:
+        return False
+
+
+def Fasta_Parser(File):
+    """This function returns a dictionary containing FastaId(key) and Seqs"""
+    Records = {}
+    with open(File, 'r') as F:
+        Seq=''
+        for Line in F:
+            if is_ID(Line) and len(Seq) == 0:
+                Header =Line.replace('\n', '').strip('>')
+            elif is_ID(Line) and len(Seq) > 0:
+                Records[Header]=Seq
+                Header =Line.replace('\n', '').strip('>')
+                Seq = ''
+            else:
+                Part=Line.replace('\n','')
+                Seq = Seq + Part
+        Records[Header]=Seq
+    return Records
+    F.close()
+
+
+def Insert_Trasncriptome(File, SpCode):
+    SpCode = int(SpCode)
+    Records = Fasta_Parser(File)
+    for Key in Records.iterkeys():
+        c.execute("INSERT INTO Transcrip VALUES (NULL, ?, ?, ?);", (Key, Records[Key], SpCode))
+
+
+def insert_Trasnscript(SpCode, File, Type):
+    if Type == 'AA':
+        
+
+
+print "NOW LEST START POPULATING THE DATABASE"
+print "FIRST LETS GET IN THE ORIGINAL TRANSCRIPTOME"
+print "ADD TRANSDECODER PEP FILE PER SPECIES"
+print "ADD THE TRANSDECODER  CDS FILE"
+
+print ""
+
+
