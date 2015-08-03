8fa381e0dedb937dd32a1d6d97e64c13273e781c
ballesterus <chuuuuy@gmail.com>
2015-08-02 21:17:52 -0400

    playing with sdesc

--- SplitsExperimental.py
+++ SplitsExperimental.py
@@ -6,25 +6,22 @@ import argparse
 
 
 #GLOBAL VARIABLE. MODIFY IF NEEDED
-sep=''
-gsep=''
+sep='|'
+gsep='\|'
 #CLASS AND FUNTION DEFINITION
 
 class newick():
     def __init__(self, N):
         self.leaves = get_leaves(N)
+        self.otus = set()
         self.newick = N
-        self.splits[]
-        new_splits(N)
-        S_0=split()
-        S_0.vec = self.leaves
-        self.splits.append(S_0)
-        
-
+        self.splits=[]
+        for leaf in self.leaves:
+           self.otus.add(leaf.split(sep)[0]) 
         
 class split():
     def __init__(self):
-        self.vec=None
+        self.vec =None
         self.covec=None
         self.branch_length=None
         self.support=None
@@ -35,47 +32,21 @@ class myPhylo():
     '''A class for newick trees'''
     def __init__(self, N):
         self.leaves = get_leaves(N)
-        self.Dict = {} # dictionary of OTU (keys), and the Unique identifiers in the newick
-        self.splits = split_decomposition(N)
+        self.sp_seqs = {} # dictionary of OTU (keys), and the Unique identifiers in the newick
+        self.splits = []
         self.ortho=[]
-        self.costs={} # Dictionary of cost for inparalog evaluation'
+        self.costs={} # Dictionary of leaf cost for inparalog evaluation'
         self.newick = N
         for leaf in self.leaves:
             otu =leaf.split(sep)[0]
             uid =leaf.split(sep)[1]
-            if otu not in self.Dict:
-                self.Dict[otu] = [] 
-            self.Dict[otu].append( uid )
+            if otu not in self.sp_seqs:
+                self.sp_seqs[otu] = [] 
+            self.sp_seqs[otu].append( uid )
             self.costs[leaf] = 1.0
 
-def new_splits_decom(T):
-    #new split  fuction using regex
-    reg= '\(([^\(\)]+)\)([0-9:\.]+)'
-    base = re.findall(reg, T.newick)
-    count=1
-    for i in base:
-        S_name = 'S_'+ str(count)
-        S_name =split()
-        S_name.vec = get_leaves(i)
-        S_name.covec= complemet(A.vec, T.leaves)
-        if len(i)!=1:
-            parts = i[1].split(':')
-            if len(parts[0]) != 0:
-                S_name.supp= parts[0]
-            if len(parts[1]) != 0:
-                S_name.branch_lengh=parts[1]
-        T.splits.append(S_name)
-        count +=1
-    for leaf in T.leaves:
-        S_name = 'S_'+ str(count)
-        S_name.vec=leaf
-        S_name.covec=complement(leaf, T.leaves)
-        parts = re.findall(leaf + '([0-9:\.]+)', T.newick)
-        S_name.branch_leenght=parts
-        T.splits.append(S_name)
-        count += 1
-    
 
+            
 def get_leaves(String):
     pattern = r"[(,]([A-Z _ a-z 0-9]+" + gsep + r"[0-9 A-Z a-z _ ]+)[:,)]"
     Leaves =re.findall(pattern, String)
@@ -88,21 +59,21 @@ def complement(Sub, Whole):
             complement.append(i)
     return complement
 
-def split_decomposition(newick):
-    'returns a list of splits'
-    leaves = get_leaves(newick)
-    splits = []
+def split_decomposition(Tree):
+    'returns an unordered collection of splits class objects'
+    newick = Tree.newick
+    leaves = Tree.leaves
     P = {}
-    id =0
+    ids = 0
     idc =0
     Pos =0
     closed = []
     for l in newick:
         if l == '(':
-            id +=1
-            P[id]=[Pos]
+            ids +=1
+            P[ids]=[Pos]
         elif l ==')':
-            idc = id
+            idc = ids
             while idc in closed and idc != 0:
                 idc = idc-1
                 #print idc
@@ -111,30 +82,44 @@ def split_decomposition(newick):
         Pos+=1
 #    print P
     vecIns = []
-    for Key in P.iterkeys():#Find splits inmplied bt the parenthesis in newick
-        vec=newick[P[Key][0]: P[Key][1]]
-        vec= get_leaves(vec)
-        coVec = complement(vec,leaves)
-        if sorted(vec) not in vecIns and sorted(coVec) not in vecIns:
-            vecIns.append(sorted(vec))
-            vecIns.append(sorted(coVec))
-            vec.append('&')
-            vec = vec + coVec
-            split = (','.join(vec))
-            splits.append(split)
-    
+    for Key in P.iterkeys():#Find splits implied by the parenthesis in newick
+        r_vec=newick[P[Key][0]: P[Key][1]]
+        vec = get_leaves(r_vec)
+        covec = complement(vec, Tree.leaves)
+        if not any (sorted(vec) == sorted(S.vec) or sorted(vec) == sorted(S.covec) for S in Tree.splits):
+            mySplits = split()
+            mySplits.vec = vec
+            mySplits.covec = covec
+            exp = re.escape(r_vec) + r'\)([0-9\.]+\:[0-9\.]+)'
+            BranchVal=re.findall(exp, Tree.newick)
+            if len(BranchVal) == 1:
+                mySplits.branch_lenght = BranchVal[0].split(':')[1]
+                mySplits.support = BranchVal[0].split(':')[0]
+            elif len(BranchVal) ==0:
+                print 'Error: This branch  has no values'
+            else:
+                print 'Error: The branch occurs %d times in the tree' % len(BranchVals)
+            Tree.splits.append(mySplits)
+        else:
+            print 'yop'
     for leaf in leaves:
-        vec=[leaf]
-        coVec = complement(vec,leaves)
-        if sorted(vec) not in vecIns and sorted(coVec) not in vecIns:
-            vecIns.append(sorted(vec))
-            vecIns.append(sorted(coVec))
-            vec.append('&')
-            vec = vec + coVec
-            split = (','.join(vec))
-            splits.append(split)
-    return splits
-
+        vec=leaf
+        covec = complement(vec,leaves)
+        if any (leaf == S.vec or leaf == S.covec for S in Tree.splits):
+            mySplit =split()
+            mySplits.vec = vec
+            mySplits.covec = covec
+            exp = re.escape(leaf) + r'\:([0-9\.]+)'
+            BranchVal =  re.findall(exp, Tree.newick)
+            if len(BranchVal) == 1:
+                mySplits.branch_lenght = BranchVal[0]
+            elif len(BranchVal) == 0:
+                print 'The input tree has no branch values'
+            else:
+                print 'The terminal: %s  occurs %d times in the tree' % (leaf, len (BranchVals))
+            Tree.splits.append(mySplit)
+        else:
+            print 'yap'
 
 def deRedundance(LoL):
     '''Takes a list of list and returns a list where no list is a subset of the others'''
