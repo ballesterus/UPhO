b2f882b678babaee2ebf900a5b60d4eca37dfb24
ballesterus <chuuuuy@gmail.com>
2015-08-07 18:16:26 -0400

    better blasthelper and blastclustering .. in work

--- BlastResultsCluster.py
+++ BlastResultsCluster.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-
-from sys import argv
+import argparse
 import os
 import re
-import pandas
-import matplotlib.pyplot as plt
-from Bio import SeqIO
+
+
+parser = argparse.ArgumentParser(description='This scrip produces clusters of homologs from a csv formatted blast output file.')
+parser.add_argument('-in', dest = 'input', type = str, default= 'None', help = 'Blast output file to process')
+parser.add_argument('-s', dest= 'separator', type =str, default= 'False', help ='Custom character separating the otu_name from the sequence identifier')
+parser.add-argument('-r' dest= 'type',  type =str, default= 'r', help ='Specify is cluster can contain more than one sequnce per species (r) or only single copy (sc) clusters')
+parser.add-argument('-mcl' dest= 'mcl' type = bool, default= 'False', help = 'When true, a "abc" filer is produce for use in the Markov Cluster Algorithm.')
+parser.add-argument('-e' dest='expectation', type=str, default = '1e-5', help ='Additional expectation value trhreshold, default 1e-5')
+parser.add-argument('-m' desr='minTaxa', type=str, default = '4', help = 'minimum number of different taxa to keep in each cluster')
+args = parser.parse_args()
+
+
 #Global variables
 
-sep='|'
-
-if sep in ["|", "^", ".", "$", "\", '?', '+', '*']:
-	re_sep = '\\' + sep
-else:
-	re_sep = sep
-
+sep=args.separator
+gsep =re.escape(sep)
 
 #Function definitions
+
 def mcl_abc(blastout, expectation):
         """Create input file abc for mcl,from a csv blast out and a expectation file """
         outName = 'mcl_%s.abc' % str(expectation)
@@ -32,21 +36,10 @@ def mcl_abc(blastout, expectation):
         myOut.close()
 
 
-def plot_eval(blastout):
-        """This fuction creates takes as an argument a file name (str) of a csv format  Blast+ output and creates a histogram of the frequency of eValues""
-        """
-        file = pandas.read_csv(blastout, names=['queryId', 'subjectId', 'percIdentity', 'alnLength', 'mismatchCount', 'gapOpenCount', 'queryStart', 'queryEnd', 'subjectStart', 'subjectEnd', 'eVal', 'bitScore'])
-        eValues = list(file.eVal)
-        plt.hist(eValues, bins = 20, )
-        plt.title(" E values Histogram")
-        plt.xlabel("EValue")
-        plt.ylabel("Frequency")
-        plt.show()
-
 
 def clusters(blastout, expectation):
         """This function take two arguments: 1) the blast csv output, and 2) an E Value threshold for the formation of clusters. The output is text file with the identifiers of the sequenes clustered  on a single line, a hidden parameter in this function is the alignment lenght, Im using 50 ut can be  modified."""
-	myOut = open("cluster_" +  str(expectation) + '.txt', 'w')
+	myOut = open("cluster_%s.txt", 'w' %expectation)
 	in_file = open(blastout, 'r')
 	n = 1
 	previous_query = "none"
@@ -68,7 +61,7 @@ def non_redundant(reference, minTaxa):
 	outFile =open("non_redundantOG.txt", "w")
         SetsInspected = []
         for line in inFile:
-                spp = re.findall(r'[A-Z]_[a-z]+', line)
+                spp = re.findall(r'([A-Z_a-z0-9]+)%s', line %gsep)
                 SeqIds = line.strip('\n').split(', ')
                 nr = set(spp)
                 if len(spp) == len(nr) and len(spp) >= int(minTaxa) and sorted(SeqIds) not in SetsInspected:
@@ -81,11 +74,11 @@ def non_redundant(reference, minTaxa):
 def redundant(reference, minTaxa):
         """Proudeces homolog-groups with at least N different OTU's, allowing redundancy but removing groups made of exclusively one OTU """
         inFile = open(reference, "rw")
-	outFile =open("redundantsOG_%s.txt", "w" %reference)
+	outFile =open("redundants_%s.txt", "w" %reference)
 
         SetsInspected = []
         for line in inFile:
-		spp = re.findall(r'[A-Z0-9_a-z]+' + re_sep, line)
+		spp = re.findall(r'[A-Z0-9_a-z]+%s', line %gsep)
 		SeqIds = line.strip('\n').split(', ')
                 nr = set(spp)
                 if len(nr) >= int(minTaxa) and sorted(SeqIds) not in SetsInspected:
@@ -95,12 +88,13 @@ def redundant(reference, minTaxa):
 
 
 def retrieve_fasta(in_file, Outdir, Type, Reference):
+	from Bio import SeqIO
         """ Takes a series of sequence comma separated Identifiers from orthogroups (one per line), and produces fasta files for each orthoGroup (line) """
         handle = open(in_file, 'r')
         if not os.path.exists(Outdir):
                 os.makedirs(Outdir)
         else:
-                print 'The output dir already exist!'
+                print 'The output directory already exist!'
         OG_number = 0
         seqSource = SeqIO.to_dict(SeqIO.parse(open(Reference), 'fasta'))
         for line in handle:
@@ -119,3 +113,18 @@ def retrieve_fasta(in_file, Outdir, Type, Reference):
                                 SeqIO.write(seqSource[seqId], OG_outfile, 'fasta')
 			print "successfully created %s" % OG_filename 
 			OG_outfile.close()
+
+
+
+#MAIN
+
+#MAIN
+if __name__ == "__main__":
+	clusters(args.input, args.expectation)
+	if args.type == 'r':
+		redundant('clusters_%s.txt', args.minTax % args.expectation)
+		retrive_fasta(,'r_clusters', 'bcl' )
+		
+	elif args.type =='sc':
+		non_redundant('clusters_%s.txt', args.minTax % args.expectation)
+	
--- Blast_helper.sh
+++ Blast_helper.sh
@@ -1,28 +1,40 @@
 #!/bin/sh
+############################
+#Usage:
+#
+# Blast_helper.sh <input.fasta>
+#
+#Requires:
+#
+#    -gnu parallel
+#    -blast+
+# 
+############################
 
 #Global variable definitions
-query=
-database=
-results=
+
 ldb_path='local_db'
 db_type='prot'
-input=
+input=$1
 #Defining function
 
 function CreateBlastDB()
 {
     if ! [ -d $ldb_path ]
-    then mkdir $ldb_path
+    then mkdir $ldb_path;
+	echo 'Creating local blast database at ' ldb_path  
     fi
-    makeblastdb -dbtype $db_type -in $input -input_type 'fasta'-out 'local_db/'$input
+    makeblastdb -dbtype $db_type -in $input -input_type fasta -out local_db/localDB
 }
 
 function AllvsAll ()
 {
-    echo "In construction"
+    echo 'Starting All vs All'
+    cat $input | parallel   --block 100k --pipe --recstart '>' blastp -evalue 0.01 -outfmt 10 -db local_db/localDB -query - > AllvsAll_results.ocsv
+
 }
 
-
-
 ### Main
-$(CreateBlastDB)
+CreateBlastDB
+AllvsAll
+exit $?
