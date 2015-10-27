3b0ba938b2a91c94646e9f4888a4e16a02a2bdf3
ballesterus <chuuuuy@gmail.com>
2015-10-27 14:41:09 -0400

    fixing naming to preserve annotations

--- minreID.py
+++ minreID.py
@@ -8,9 +8,6 @@ script, infile, SP_code, sep  = argv
 
 inp_F = open(infile, 'r')
 out_name = SP_code + "_withid.fst"
-#species = raw_input(">Type the species name NO SPACES:")
-#species_Name = "|" + species
-#type_of_seq=raw_input('>What type of sequence is this?')
 out_F = open(out_name, 'w')
 counter = 1
 
@@ -20,7 +17,7 @@ for line in inp_F:
     unique_id= '>' + SP_code + sep + idNum
     if id_Line:
         nl0=re.sub(r'cds\.', '', line) # remove cds prefix
-        nl1 = re.sub(r'\|.+', '', nl0) # remove additional annotations
+        nl1 = re.sub(r'\s.+', '', nl0) # remove additional annotations
         nl2 = re.sub(r'^>', unique_id, nl1) # add unique identifier 
         out_F.write(nl2)
         counter += 1 
