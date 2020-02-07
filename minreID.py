1ca937d80cfe90c1090abd4447b7f3879592f838
ballesterus <chuuuuy@gmail.com>
2020-02-07 17:12:37 -0600

    minreid migrated to Python3

--- minreID.py
+++ minreID.py
@@ -1,24 +1,27 @@
-#!/usr/bin/env python
-"""Usage: python minreID.py  <inputfile> SP_CODE  Separator"""
+#!/usr/bin/env python3
+
 import os
 import re
 from sys import argv
 
-script, infile, SP_code, sep  = argv
+if len(argv) == 4:
+    script, infile, SP_code, sep  = argv
+    inp_F = open(infile, 'r')
+    out_name = SP_code + "_withid.fst"
+    out_F = open(out_name, 'w')
+    counter = 1
 
-inp_F = open(infile, 'r')
-out_name = SP_code + "_withid.fst"
-out_F = open(out_name, 'w')
-counter = 1
-
-for line in inp_F:
-    id_Line= re.search(r'^>', line)
-    idNum = format(counter, '06d')
-    unique_id= '>' + SP_code + sep + idNum + sep
-    if id_Line:
-        nl1 = re.sub(r'\s.+', '', line) # remove additional annotations
-        nl2 = re.sub(r'^>', unique_id, nl1) # add unique identifier 
-        out_F.write(nl2)
-        counter += 1 
-    else:
-        out_F.write(line)
+    for line in inp_F:
+        id_Line= re.search(r'^>', line)
+        idNum = format(counter, '06d')
+        unique_id= '>' + SP_code + sep + idNum + sep
+        if id_Line:
+            nl1 = re.sub(r'\s.+', '', line) # remove additional annotations
+            nl2 = re.sub(r'^>', unique_id, nl1) # add unique identifier 
+            out_F.write(nl2)
+            counter += 1 
+        else:
+            out_F.write(line)
+else:
+    print("ERROR: Wrong number of arguments, see Usage below.")
+    print ("""Usage: minreID.py  <INPUT_FASTA> <SP_CODE>  <SEPARATOR>""")
